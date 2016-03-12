0ced852044b43be565eee4e3d3747253ccef2b1a
jinhaoplus <rowin2013@gmail.com>
2016-03-12 13:56:27 +0800

    debug

--- src/com/jinhaoplus/oj/service/langCore/impl/JavaCoreServiceImpl.java
+++ src/com/jinhaoplus/oj/service/langCore/impl/JavaCoreServiceImpl.java
@@ -81,9 +81,10 @@ public class JavaCoreServiceImpl implements LangCoreService {
 			CommonMessage message = null;
 			ProcessBuilder processBuilder;
 			String javaDir = path.substring(0,path.lastIndexOf("/")+1);
-			processBuilder = new ProcessBuilder("java","Test");
-			System.out.println(processBuilder.directory().getAbsolutePath());
+			processBuilder = new ProcessBuilder("java","-cp",".","Test");
+			
 			processBuilder.directory(new File(javaDir));
+			System.out.println(processBuilder.directory().getAbsolutePath());
 			processBuilder.redirectErrorStream(true);
 
 			
